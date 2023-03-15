80968359c6d8ff0ee688f1c49e1436a6f73c00ba
leekyuyoung20230313 <127713278+leekyuyoung20230313@users.noreply.github.com>
2023-03-15 15:48:12 +0900

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,8 @@
+numbers = 1+2+3+4+5+6 # 
+print(numbers) #
+print("numbers 변수의 값은 : ",numbers) #
+numbers = 100 #
+print(numbers) #
+print("원하는 아무 값이나 입력하세요")
+numbers = int( input() )
+print("입력하신값은 : ",numbers)
---
+++ 003.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    print("하하하")
+    
+sum = 0    
+for i in range(1,11):
+    sum += i
+print("1부터 10까지 더하면",sum)    
+        
